e1e4dc139918a0cedbd149bdaacbc5fb8fe83e82
Ubuntu <ubuntu@ip-172-31-81-78.ec2.internal>
2023-11-15 20:49:02 +0000

    Run code with approprotate input values

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,34 +1,37 @@
 import java.util.Scanner;
 
-class Dec2Hex
-{
+class Dec2Hex {
 
-    public static int Arg1;
-
-    public static void main(String args[])
-    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+    public static void main(String args[]) {
+        if (args.length == 0) {
+            System.err.println("Error: No input argument provided.");
+            System.exit(1); // Exit with an error code
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        try {
+            int num = Integer.parseInt(args[0]);
+            if (num < 0) {
+                System.err.println("Error: Input value must be a non-negative integer.");
+                System.exit(1); // Exit with an error code
+            }
 
+            char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+            int rem;
+            String hexadecimal = "";
+            System.out.println("Converting the Decimal Value " + num + " to Hex...");
+
+            while (num != 0) {
+                rem = num % 16;
+                hexadecimal = ch[rem] + hexadecimal;
+                num = num / 16;
+            }
+
+            System.out.println("Hexadecimal representation is: " + hexadecimal);
+
+        } catch (NumberFormatException e) {
+            System.err.println("Error: Input must be a valid integer.");
+            System.exit(1); // Exit with an error code
+        }
     }
 }
 
-
-/*
-Test build trigger
-Test SonarQube
-
-*/
