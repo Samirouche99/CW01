43ce791c461fe6abd029625da6bfdf0d9ef967ec
Ubuntu <ubuntu@ip-172-31-81-78.ec2.internal>
2023-11-18 12:42:52 +0000

    Update Junit test file and Dec2Hex script

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,27 +1,44 @@
-import java.util.Scanner;
+// This class converts a decimal integer to its hexadecimal representation
+public class Dec2Hex {
 
-class Dec2Hex
-{
-
-    public static int Arg1;
-
-    public static void main(String args[])
-    {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        while(num != 0)
-        {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+    // Main method to be executed when running the program
+    public static void main(String args[]) {
+        // Check if an input is provided
+        if (args.length == 0) {
+            System.err.println("Error: No input provided. Please provide an integer as input.");
+            System.exit(1); // Exit with an error code
         }
 
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        try {
+            // Parse the input as an integer
+            int input = Integer.parseInt(args[0]);
+            // Convert the integer to hexadecimal
+            String hexadecimal = convertToHex(input);
+            // Print the hexadecimal representation
+            System.out.println("Hexadecimal representation is: " + hexadecimal);
+        } catch (NumberFormatException e) {
+            // Handle invalid input (non-integer)
+            System.err.println("Error: Invalid input. Please provide a valid integer.");
+            System.exit(1); // Exit with an error code
+        }
+    }
 
+    // Converts a decimal integer to its hexadecimal representation
+    public static String convertToHex(int decimal) {
+        // Array representing hexadecimal digits
+        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+        int rem;
+        String hexadecimal = "";
+
+        // Loop to perform the conversion
+        while (decimal != 0) {
+            rem = decimal % 16;
+            hexadecimal = ch[rem] + hexadecimal;
+            decimal = decimal / 16;
+        }
+
+        // Return the hexadecimal representation
+        return hexadecimal;
     }
 }
+
--- Junit.java
+++ Junit.java
@@ -1,10 +1,43 @@
-import java.util.Scanner;
+import static org.junit.Assert.assertEquals;
 
-class Junit
-{
-    public static void main(String args[])
-    {
-        
-        System.out.println("Hello World");
+public class Junit {
+
+    public static void main(String[] args) {
+        // Run the test methods
+        testConversionWithZero();
+        testConversionWithPositiveNumber();
+        testConversionWithNonIntegerInput();
+        // Add more test methods as needed
     }
+
+    // Test method for converting zero to hexadecimal
+    public static void testConversionWithZero() {
+        // Perform the conversion
+        String result = Dec2Hex.convertToHex(0);
+        // Check the result and print the status
+        System.out.println("Test Conversion with Zero: " + ("".equals(result) ? "Passed" : "Failed"));
+    }
+
+    // Test method for converting a positive number to hexadecimal
+    public static void testConversionWithPositiveNumber() {
+        // Perform the conversion
+        String result = Dec2Hex.convertToHex(26);
+        // Check the result and print the status
+        System.out.println("Test Conversion with Positive Number: " + ("1A".equals(result) ? "Passed" : "Failed"));
+    }
+
+    // Test method for handling non-integer input
+    public static void testConversionWithNonIntegerInput() {
+        try {
+            // Try to convert a non-integer input
+            Dec2Hex.convertToHex(Integer.parseInt("invalid"));
+            // If no exception is thrown, print failure status
+            System.out.println("Test Conversion with Non-Integer Input: Failed. Expected NumberFormatException, but no exception was thrown.");
+        } catch (NumberFormatException e) {
+            // If NumberFormatException is caught, print success status
+            System.out.println("Test Conversion with Non-Integer Input: Passed");
+        }
+    }
+
 }
+
