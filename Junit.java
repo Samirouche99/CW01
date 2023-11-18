ab0b8c99e2433735bfa571bd117c3dba38ea69c0
Ubuntu <ubuntu@ip-172-31-81-78.ec2.internal>
2023-11-18 10:52:22 +0000

    Commit Junit file for testing

--- Junit.java
+++ Junit.java
@@ -1,1 +1,10 @@
-System.out.println("Hello World");
+import java.util.Scanner;
+
+class Junit
+{
+    public static void main(String args[])
+    {
+        
+        System.out.println("Hello World");
+    }
+}
