7e0a3bdd7f0c0b73040ae668c6da5854ac76c3cb
Ubuntu <ubuntu@ip-172-31-81-78.ec2.internal>
2023-11-19 22:52:16 +0000

    Fix code smell v2

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,3 +1,4 @@
+
 import java.util.logging.Logger;
 
 // This class converts a decimal integer to its hexadecimal representation
@@ -21,22 +22,23 @@ public class Dec2Hex {
             Logger.getLogger(Dec2Hex.class.getName()).severe("Error: Invalid input. Please provide a valid integer.");
             System.exit(1); // Exit with an error code
         }
+    } // Close the main method here
+
     public static String convertToHex(int decimal) {
-    // Array representing hexadecimal digits
-    char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-    int rem;
-    StringBuilder hexadecimal = new StringBuilder();
+        // Array representing hexadecimal digits
+        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        int rem;
+        StringBuilder hexadecimal = new StringBuilder();
 
-    // Loop to perform the conversion
-    while (decimal != 0) {
-        rem = decimal % 16;
-        hexadecimal.insert(0, ch[rem]); // Insert at the beginning of the StringBuilder
-        decimal = decimal / 16;
+        // Loop to perform the conversion
+        while (decimal != 0) {
+            rem = decimal % 16;
+            hexadecimal.insert(0, ch[rem]); // Insert at the beginning of the StringBuilder
+            decimal = decimal / 16;
+        }
+
+        // Return the hexadecimal representation
+        return hexadecimal.toString();
     }
-
-    // Return the hexadecimal representation
-    return hexadecimal.toString();
 }
-}
-
 
